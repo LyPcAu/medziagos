46744d1ea1676085f94d42fb7c37c489f9263c46
Justinas Bagdonas <nikodemasbagdonas@gmail.com>
2020-10-17 18:56:41 +0300

    Added base laravel files

---
+++ app/Http/Controllers/CalcController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Material;
+
+class CalcController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+	
+	public function create()
+    {
+        //
+    }
+	
+	public function store()
+    {
+        //
+    }
+	
+	public function show()
+    {
+        //
+    }
+	
+	public function edit()
+    {
+        //
+    }
+	
+	public function update()
+    {
+        //
+    }
+	
+	public function destroy()
+    {
+        //
+    }
+}
